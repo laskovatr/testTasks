54c2809c1b58b5319e36e3355be138b4c9807fc5
laskovatr <65371301+laskovatr@users.noreply.github.com>
2023-03-03 22:45:15 +0300

    Update SQL2.sql

--- SQL2.sql
+++ SQL2.sql
@@ -2,7 +2,6 @@
 -- in PostGIS 3, postgis_raster is a separate extension from postgis
 -- so do this in addition if you are experimenting PostGIS 3+
 CREATE EXTENSION postgis_raster;
-
 CREATE EXTENSION postgis_sfcgal;
 CREATE EXTENSION address_standardizer;
 CREATE EXTENSION fuzzystrmatch;
@@ -12,11 +11,8 @@ CREATE EXTENSION postgis_tiger_geocoder;
 ALTER TABLE geo_test.test_point
 ADD COLUMN geom geometry(POINTZ);
 
-insert into geo_test.test_point(geom) values (ST_GeomFromText('POINTZ(1 0 0)')); 
-
 UPDATE geo_test.test_point set geom = ST_GeomFromText('POINTZ(1 0 0)') WHERE id = 1;
 UPDATE geo_test.test_point set geom = ST_GeomFromText('POINTZ(0 1 0)') WHERE id = 2;
 UPDATE geo_test.test_point set geom = ST_GeomFromText('POINTZ(0 0 1)') WHERE id = 3;
 
 select ST_AsGeoJSON(t.*) geom from geo_test.test_point as t(geom);
-
